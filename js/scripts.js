9c3d12f1c2c797c39bac4b620dedd92c0b3ae888
Julian DW <klerpson@gmail.com>
2022-10-20 14:30:54 -0500

    updates design

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+// <!-- Global site tag (gtag.js) - Google Analytics -->
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-JQ0EGSYDTF');
+// <!-- End gtag -->
+
+  (function (w, d, s, l, i) {
+    w[l] = w[l] || [];
+    w[l].push({
+      'gtm.start': new Date().getTime(),
+      event: 'gtm.js'
+    });
+    var f = d.getElementsByTagName(s)[0],
+      j = d.createElement(s),
+      dl = l != 'dataLayer' ? '&l=' + l : '';
+    j.async = true;
+    j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+    f.parentNode.insertBefore(j, f);
+  })(window, document, 'script', 'dataLayer', 'GTM-MW9PB8L');
+
+
+// Clarity tracking code for 'https://gurcoff.com/'
+  (function (c, l, a, r, i, t, y) {
+    c[a] = c[a] || function () {
+      (c[a].q = c[a].q || []).push(arguments)
+    };
+    t = l.createElement(r);
+    t.async = 1;
+    t.src = "https://www.clarity.ms/tag/" + i + "?ref=bwt";
+    y = l.getElementsByTagName(r)[0];
+    y.parentNode.insertBefore(t, y);
+  })
+  (window, document, "clarity", "script", "9dvv2gw4up");
+
+function loadScript(a){var b=document.getElementsByTagName("head")[0],c=document.createElement("script");c.type="text/javascript",c.src="https://tracker.metricool.com/resources/be.js",c.onreadystatechange=a,c.onload=a,b.appendChild(c)}loadScript(function(){beTracker.t({hash:"a11a055d8d9feee453481f347a31f2e2"})});
